3a84c1568da4a8930e1ccc11ab8feb9c2d6341a2
robaita <robaita2021@gmail.com>
2024-08-08 14:45:48 +0530

    updated config file

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,17 +5,14 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // You can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
-    siteImage: `/banner.jpg`,
+    siteTitle: `Robaita`,
+    siteTitleAlt: `Robaita | Robotics and Artificial Intelligence Training Academy`,
+    siteHeadline: `Robaita | Robotics and Artificial Intelligence Training Academy`,
+    siteUrl: `https://robaita.com`,
+    siteDescription: `Robotics and Artificial Intelligence Training Academy, Machine Learning, Computer Vision, Deep Learning, Natural Language Processing, Generative AI, Artificial Intelligence`,
+    siteImage: `src/assets/images/robaita.PNG`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `Dr. Avinash Kumar Singh`,
   },
   trailingSlash: `always`,
   plugins: [
