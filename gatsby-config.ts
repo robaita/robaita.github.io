ece7107f1b7b169d55e3e047ec0b3e88d65615fb
robaita <robaita2021@gmail.com>
2024-08-08 14:50:18 +0530

    updated config file

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -36,12 +36,16 @@ const config: GatsbyConfig = {
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            name: `Github`,
+            url: `https://github.com/robaita`,
           },
           {
             name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            url: `https://robaita.com`,
+          },
+          {
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/dr-avinash-kumar-singh-2a570a31/`,
           },
         ],
       },
@@ -55,9 +59,9 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Robaita | Robotics and Artificial Intelligence Training Academy`,
+        short_name: `robaita`,
+        description: `Robotics and Artificial Intelligence Training Academy, Machine Learning, Computer Vision, Deep Learning, Natural Language Processing, Generative AI, Artificial Intelligence`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
